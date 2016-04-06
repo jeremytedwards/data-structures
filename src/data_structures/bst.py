d821fa86d0038dde25245c7d3832e178863576ee
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-06 09:52:42 -0700

    Formatted doc strings

--- src/data_structures/bst.py
+++ src/data_structures/bst.py
@@ -14,26 +14,40 @@ class Tree:
 
     def insert(self, val):
         '''
-        Will insert the value val into the BST. If val is already present, it will be ignored.
+        will insert the value val into the BST. If val is already present,
+        it will be ignored.
         '''
         pass
 
     def contains(self, val):
-        '''will return True if val is in the BST, False if not.'''
+        '''
+        will return True if val is in the BST, False if not.
+        '''
         pass
 
     def size(self):
-        '''will return the integer size of the BST (equal to the total number of values stored in the tree). It will return 0 if the tree is empty.'''
+        '''
+        will return the integer size of the BST (equal to the total number of values
+        stored in the tree). It will return 0 if the tree is empty.
+        '''
         pass
 
     def depth(self):
-        '''will return an integer representing the total number of levels in the tree. If there is one value, the depth should be 1, if two values it will be 2, if three values it may be 2 or three, depending, etc.'''
+        '''
+        will return an integer representing the total number of levels in the tree.
+        If there is one value, the depth should be 1, if two values it will be 2, if t
+        hree values it may be 2 or three, depending, etc.
+        '''
         pass
 
     def balance(self):
-        '''will return an integer, positive or negative that represents how well balanced the tree is. Trees which are higher on the left than the right should return a positive value, trees which are higher on the right than the left should return a negative value. An ideally-balanced tree should return 0.'''
+        '''
+        will return an integer, positive or negative that represents how well balanced
+        the tree is. Trees which are higher on the left than the right should return a
+        positive value, trees which are higher on the right than the left should return
+        a negative value. An ideally-balanced tree should return 0.
+        '''
         pass
-
 
     def get_dot(self):
         """return the tree with root 'self' as a dot graph for visualization"""
