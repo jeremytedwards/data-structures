ca635082a3f8bfc1d9c8cb09eb2a7a6f312dd15b
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-18 14:36:41 -0700

    Added the _hash helper method to take a string or int

--- src/data_structures/hash_table.py
+++ src/data_structures/hash_table.py
@@ -3,19 +3,20 @@
 
 class HashTable(object):
     def __init__(self, hash_size=1024):
-        self.size = hash_size
+        self._size = hash_size
+        self.storage = [[] for i in range(hash_size)]
 
-    @staticmethod
-    def get(key):
+    def get(self, key):
         """returns the value stored with the given key"""
         pass
 
-    @staticmethod
-    def set(key, val):
+    def set(self, key, val):
         """stores the given val using the given key"""
         pass
 
-    @staticmethod
-    def _hash(key):
-        """hashes the key provided(note that this is an internal api)"""
-        pass
+    def _hash(self, key):
+        """
+        hashes the key provided(note that this is an internal api)
+        this method prefers a string.
+        """
+        return sum([ord(c) for c in str(key)]) % self._size
