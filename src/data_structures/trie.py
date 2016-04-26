6ce24e14b50f419c935aa48e23976586400885e8
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-26 02:12:31 -0700

    Methods stubbed for trie class

--- src/data_structures/trie.py
+++ src/data_structures/trie.py
@@ -1,1 +1,18 @@
 # coding=utf-8
+
+
+class trie():
+    def __init__(self):
+        pass
+
+    def insert(self, token):
+        """
+        Will insert the value token into the trie. If character in token is already present,
+        it will be ignored.
+        """
+
+    def contains(self, token):
+        """
+        Will return True if token is in the trie, False if not.
+        """
+        pass
