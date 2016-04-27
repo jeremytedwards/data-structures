c29d5a5b84594c28a84d804b64c308b6ceef9293
Jeremy Edwards <jeremytedwards@gmail.com>
2016-04-27 16:20:35 -0700

    Revisions to README, spacing Initial Passes on methods and tests for the trie class

--- src/data_structures/trie.py
+++ src/data_structures/trie.py
@@ -1,18 +1,51 @@
 # coding=utf-8
 
 
-class trie():
+class Trie(object):
+    """
+    Example Trie with two token inserts
+        token = baa
+        token = ball
+        { 'b': { 'a': { 'a': { '$' : '$' },
+                        'l': { 'l': { '$' : '$' }},
+        }    }    }
+    """
     def __init__(self):
-        pass
+        self._key = {}
+        self._value = "*"
 
     def insert(self, token):
         """
         Will insert the value token into the trie. If character in token is already present,
         it will be ignored.
         """
+        if token:
+            try:
+                test_value = self._key[token[0]]
+                self._key[token[0]].insert(token[1:])
+            except KeyError:
+                self._key[token[0]] = Trie()
+                if token[1]:
+                    self._key[token[0]].insert(token[1:])
+                else:
+                    self._key[token[0]] = {"$": "$"}
 
     def contains(self, token):
         """
         Will return True if token is in the trie, False if not.
+        """
+        if token:
+            test_value = self._key.get(token[0], default=None)
+            if test_value is None:
+                return False
+            elif test_value == "$":
+                return True
+            else:
+                self._key[token[0]].contains(token[1:])
+
+    def traversal(self, start):
+        """"
+        Perform a full depth-first traversal of the graph beginning at start.
+        Return: a generator containing all tokens in the trie.
         """
         pass
--- tests/test_trie.py
+++ tests/test_trie.py
@@ -1,1 +1,32 @@
 # coding=utf-8
+
+import pytest
+
+TRIE_TABLE = {
+    ("baa", {"b": {"a": {"a": {"$": "$"}}}}),
+    # ("bat", {'b': {'a': {'t': {'$': '$'}}}}),
+    # ("ball", {'b': {'a': {'l': {'l': {'$': '$'}}}}}),
+}
+
+
+@pytest.mark.parametrize("test, result", TRIE_TABLE)
+def test_insert(test, result):
+    from data_structures.trie import Trie
+    test_trie = Trie()
+    test_trie.insert(test)
+    assert test_trie == result
+
+
+# CONTAINS_TABLE = {
+#     ("baa", baa),
+#     ("bat", "bat"),
+#     ("ball", "ball"),
+# }
+#
+#
+# @pytest.mark.parametrize("test, result", CONTAINS_TABLE)
+# def test_contains(test, result):
+#     from data_structures.trie import Trie
+#     test_trie = Trie()
+#     test_trie.insert(test)
+#     assert test_trie.contains(test) == result
